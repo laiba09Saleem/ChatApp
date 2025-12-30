769194e2fac12798f377f609d1cbfc4815144de4
Laiba Saleem <laibasaleem068@gmail.com>
2025-12-30 19:34:51 +0500

    feat: set up project URL routing

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from chat.views import chat_home, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('api/auth/', include('accounts.urls')),
+    path('api/', include('chat.urls')),
+    path('chat/', chat_home, name='chat_home'),  # Add this
+    path('', home, name='home'),
+    # API documentation
+    path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
